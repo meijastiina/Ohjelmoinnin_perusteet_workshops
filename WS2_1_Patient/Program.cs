7956b442032399754120659f16c24d6d05c5172b
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-15 16:27:48 +0300

    Initial commit WS2.1 Patient

--- WS2_1_Patient/Program.cs
+++ WS2_1_Patient/Program.cs
@@ -6,7 +6,14 @@ namespace WS2_1_Patient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. Create a program that has two variables; patientFirstName and patientLastName.
+            // 2. Set the values for those variables to be Matti and Meikäläinen.
+            var patientFirstName = "Matti"; // This variable is a string
+            string patientLastName; // At this point this variable doesn't have a value
+            patientLastName = "Meikäläinen"; // Value can be assigned at a later stage, too
+            // 3. Print out the patient’s name on one line, separated by a space. 
+            Console.WriteLine(patientFirstName + " " + patientLastName); // This is the traditional way concatenating strings
+            Console.WriteLine($"{ patientFirstName } { patientLastName}"); // In C# you can also use values of variables in a string this way (will give the same result as above).
         }
     }
 }
