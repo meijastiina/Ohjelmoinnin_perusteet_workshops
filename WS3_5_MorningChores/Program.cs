ed368c174cb5737e4258e5506e1416d78bbd09e1
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-19 13:55:09 +0300

    Initial commit WS3.5 Morning Chores

---
+++ WS3_5_MorningChores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WS3_5_MorningChores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create a program that asks if the user has completed specific morning chores (breakfast, teeth brushing, packing). 
+            Console.Write("Breakfast has been eaten: ");
+
+            // 2.Use Boolean values to store given answers.
+            bool breakfastEaten = bool.Parse(Console.ReadLine());
+            Console.Write("Teeth have been brushed: ");
+            bool teethBrushed = bool.Parse(Console.ReadLine());
+            Console.Write("Backpack has been packed: ");
+            bool backpackBacked = bool.Parse(Console.ReadLine());
+
+            // 3.	Based on the answers given the program decides whether the user is ready to go to school or not.
+            if (breakfastEaten && teethBrushed && backpackBacked) 
+            /* You can use logical operator AND (&&) here to  make sure all the conditions are fullfilled.
+             * You could also build the condition using logical operator OR (||):
+             * if (breakfastEaten == false || teethBrushed == false || backpackBacked == false)
+             * or
+             * if (!breakfastEaten || !teethBrushed || !backpackBacked )
+             */
+            {
+                Console.WriteLine("Off to school you go!");
+            } else
+            {
+                Console.WriteLine("Hold your horses, something's missing!");
+            }
+        }
+    }
+}
