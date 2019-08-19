e3952dfcffe9eae1b9d0ecdeb715ce25168e9ced
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-19 09:50:45 +0300

    Initial commit for 3.3 Age

---
+++ WS3_3_Age/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WS3_3_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Create a program that asks for user’s age.
+            Console.Write("Please enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            // 2.If the given age is greater than 17, print out: “You are of age.”,
+            if (age > 17)
+            {
+                Console.WriteLine("You are of age.");
+            }
+            else // otherwise print out: “You’re a minor.”.
+            {
+                Console.WriteLine("You're a minor.");
+            }
+        }
+    }
+}
