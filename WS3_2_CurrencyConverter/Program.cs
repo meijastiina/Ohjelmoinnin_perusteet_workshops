9719ceda33b999aed82b24b6a89191adc9189429
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-19 09:25:37 +0300

    Initial commit for WS3.2 Currency Converter

---
+++ WS3_2_CurrencyConverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WS3_2_CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Create a program that asks the user for the convert rate for US dollars and the amount to be converted in euros.
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*          CURRENCY CONVERTER          *");
+            Console.WriteLine("****************************************");
+            Console.Write("Please enter currency convert rate: ");
+            var rate = Console.ReadLine();
+            Console.Write("Please enter amount in euros: ");
+            var amount =  Console.ReadLine();
+            // 2.Calculate the amount of US dollars that equal the amount given in euros.
+            var convertedAmount = double.Parse(amount) / double.Parse(rate); // Console.ReadLine(); returns a string. We can't use strings in calculations -> we must parse the string into a double.
+            Console.WriteLine($"Amount in dollars: { convertedAmount } ");
+            Console.WriteLine("****************************************");
+        }
+    }
+}
