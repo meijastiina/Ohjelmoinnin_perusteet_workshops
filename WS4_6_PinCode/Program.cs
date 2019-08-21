ff374dd9d819dc71117685a8ba3f4c2594ad1232
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-21 12:18:01 +0300

    Initial commit WS 4.6 Pin Code

---
+++ WS4_6_PinCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WS4_6_PinCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks the user for PIN code. Keep asking till the correct code(3120) is entered.Use do while or while loop.
+            int pincode = 3120;
+            int enteredPin;
+            do // Do is a good choice here cause we always want to ask at least once.
+            {
+                Console.Write("PIN: ");
+                enteredPin = int.Parse(Console.ReadLine());
+            } while (enteredPin != pincode);
+            Console.WriteLine("PIN ok!");
+        }
+    }
+}
