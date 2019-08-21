60d6db5b7b9d96202eacc4966e55b67703f06aaa
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-21 09:43:32 +0300

    Initial commit - WS 3.7 Lear Year 2 - WS 4.1 Number Comparer

---
+++ WS3_7_LeapYear2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace WS_7_LeapYear2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Improve the leap year program created in the previous exercise by adding functionality also to handle the years before Gregorian calendar (1753). 
+            Console.Write("Please enter a year to be checked: ");
+            int year = int.Parse(Console.ReadLine());
+            bool isLeapYear = false;
+            if (year < 1753)
+            {
+                if (year % 4 == 0)  // 1.every year that is exactly divisible by four
+                {
+                    isLeapYear = true;
+                }
+            }
+            else
+            {
+                // From the year 1753 we have been using Gregorian calendar in which leap years are:
+                if (year % 4 == 0)  // 1.every year that is exactly divisible by four
+                {
+                    if (year % 100 == 0)  // 1.except for years that are exactly divisible by 100
+                    {
+                        if (year % 400 == 0)     // but these centurial years are leap years if they are exactly divisible by 400.
+                        {
+                            isLeapYear = true;
+                        }
+                    }
+                    else
+                    {
+                        isLeapYear = true;
+                    }
+                }
+            }
+            if (isLeapYear)
+            {
+                Console.WriteLine($"{ year } is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{ year } is NOT a leap year.");
+            }
+        }
+    }
+}
---
+++ WS4_1_NumberComparer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WS4_1_NumberComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for user’s age. Implement conditionals (if, else if, else) that works as follows:
+            Console.Write("Please give me your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            // If age is less than 0 or greater than 150 print “Invalid age!”
+            if (age < 0 || age > 150)
+            {
+                Console.WriteLine("Invalid age!");
+            }
+            // If age is less than 6 print ”Preschooler”
+            else if ( age < 6) {
+                Console.WriteLine("Preschooler");
+            }
+            // If age is 6 - 14 print ”You’re a child”
+            else if (age < 15)
+            {
+                Console.WriteLine("You're a child");
+                // If age is 14 print ”Challenging age”
+                if (age == 14)
+                {
+                    Console.WriteLine("Challenging age");
+                }
+            }
+            // If age is 16 - 18 print ”Almost grown up”
+            else if (age <= 18)
+            {
+                Console.WriteLine("Almost grown up");
+            }
+            //If age is greater than 18 but less than 30 print ”You are an adult but not experiencing midlife crisis yet” 
+            else if (age < 30)
+            {
+                Console.WriteLine("You are an adult but not experiencing midlife crisis yet");
+            }
+            // If age is greater or equal to 30 but less than 45 print ”You are middle aged”
+            else if (age >= 30 && age < 45)
+            {
+                Console.WriteLine("You are middle aged");
+            }
+            // If age is greater than 45 but under 65 print ”You still have time before retirement!”
+            else if (age > 45 && age < 65)
+            {
+                Console.WriteLine("You still have time before retirement!");
+            }
+            // Otherwise print “You’re a pensioner”
+            else
+            {
+                Console.WriteLine("You’re a pensioner");
+            }
+        }
+    }
+}
