e4c16901bc040862ef7eed4c98f102f1267f2272
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-21 10:14:13 +0300

    Initial commit WS 4.2 Email Validation

---
+++ WS4_2_EmailValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WS4_2_EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for an email address and validates it. 
+            Console.Write("Please enter an email: ");
+            string email = Console.ReadLine();
+            /* Email is valid if it fulfils all the requirements listed below: 
+             * - includes one @ sign
+             * - includes one dot(.)
+             * - is longer than 5 characters
+             * (Of course this is not a fully functional email validation but good enough for now.)
+             */
+            if (email.Contains("@") && email.Contains(".") && email.Length > 5)
+            {
+                Console.WriteLine("Email ok!");
+            } else
+            {
+                Console.WriteLine("Invalid email");
+            }
+        }
+    }
+}
