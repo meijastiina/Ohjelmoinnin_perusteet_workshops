24508543dabf4ba9055d6dcac0ade584b50f1431
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-23 19:58:38 +0300

    Initial commit WS 6.2 Lorem Ipsum

---
+++ WS6_2_LoremIpsum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WS6_2_LoremIpsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printLoremIpsumTenTimes();
+        }
+        // Create a method that prints out Lorem ipsum sentence (”Lorem Ipsum dolor sit amet, consectetur adipiscing elit. ”) ten times.
+        private static void printLoremIpsumTenTimes()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Further improve your program so that instead of printing directly in the loop in your method, it calls another method that prints the sentence.
+                printLoremIpsum(); 
+
+            }
+        }
+        
+        private static void printLoremIpsum()
+        {
+            Console.WriteLine("Lorem Ipsum dolor sit amet, consectetur adipiscing elit.");
+        }
+    }
+}
