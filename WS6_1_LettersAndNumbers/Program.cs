5b57c015173dacfc52d78c0a85bb47cdcb66854e
meijastiina <mlohiniva@hotmail.com>
2019-08-23 19:47:56 +0300

    Initial commit WS 6.1. Letters and Numbers

---
+++ WS6_1_LettersAndNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WS6_1_LettersAndNumbers
+{
+    class Program
+    {
+        // Create a program with three methods:
+        // 1.	main
+
+        static void Main(string[] args)
+        {
+            // In your main method, call first printLetters() method,
+            printLetters();
+            //  then printNumbers() 
+            printNumbers();
+            // and after that printLetters() again.
+            printLetters();
+        }
+        // 2.	printLetters() : prints letters from a-z
+        private static void printLetters()
+        {
+            Console.WriteLine("abcdefghijklmnopqrstuvwxyz");
+        }
+        // 3.	printNumbers() : prints numbers from 1-9
+        private static void printNumbers()
+        {
+            Console.WriteLine("123456789");
+        }
+    }
+}
