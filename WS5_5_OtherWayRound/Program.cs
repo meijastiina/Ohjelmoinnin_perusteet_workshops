0af092d38272851a33a4d0317697e814abfa94db
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-23 15:02:57 +0300

    Initial commit WS 5.5 Other Way Round

---
+++ WS5_5_OtherWayRound/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WS5_5_OtherWayRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for a string and then prints it out from the last character to the first. 
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            string reversedInput = "";
+            for (int i = input.Length - 1; i >= 0; i--) // Let's start from the last character and loop through to the first.
+            {
+                // You can access a character in a specific index by using square brackets (string name =”John”; name[0] =”J”).
+                reversedInput += input[i];
+            }
+            Console.WriteLine(reversedInput);
+        }
+    }
+}
