94a933b33ffa5c2f571223b4eeee6ad1247e4a37
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-23 13:33:45 +0300

    Initial commit WS 5.5 Multiplication table

---
+++ WS4_7_PrintingAMenu2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WS4_7_PrintingAMenu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            string distance = "";
+            do
+            {
+                // Print out the menu shown below. 
+                Console.WriteLine("*********************************");
+                Console.WriteLine("*\t1 - Start a car\t\t*");
+                Console.WriteLine("*\t2 - Drive a car\t\t*");
+                Console.WriteLine("*\t3 - Turn car off\t*");
+                Console.WriteLine("*\t4 - Quit game\t\t*");
+                Console.WriteLine("*********************************");
+
+                choice = int.Parse(Console.ReadLine());
+
+                // Use switch case to figure out user’s selection. Print something relevant for each selection. 
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("   __");
+                        Console.WriteLine(" _ | =\\__");
+                        Console.WriteLine("/ o____o_\\");
+                        break;
+                    case 2:
+                        Console.WriteLine("Car moving");
+                        distance += "\t";
+                        
+                        Console.Write(distance);
+                        Console.WriteLine("   __");
+                        Console.Write(distance);
+                        Console.WriteLine(" _ | =\\__");
+                        Console.Write(distance);
+                        Console.WriteLine("/ o____o_\\");
+                        break;
+                    case 3:
+                        Console.WriteLine("Car stopped");
+                        distance = "";
+                        break;
+                    case 4:
+                        Console.WriteLine("Good bye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            } while (choice != 4);
+        }
+    }
+}
---
+++ WS5_1_MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace WS5_1_MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints out multiplication tables for 1-20. Use for loops. 
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"Multiplication table for { i }:");
+                Console.WriteLine("*************************");
+                for (int j = 1; j <= 10; j++)
+                {
+                    // { j, 6 } means that we reserve 6 characters for number j and it's right aligned
+                    // { " ", 5 } just creates five empty spaces
+                    Console.WriteLine($"*{ j, 6 } * { i, 2 } = { i * j, 3 }{ " ", 5} *");
+                }
+                Console.WriteLine("*************************");
+                Console.WriteLine();
+            }
+        }
+    }
+}
