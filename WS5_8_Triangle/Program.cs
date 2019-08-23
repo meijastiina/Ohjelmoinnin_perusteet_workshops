3d7a5d0d1204d439bac358b511e52bb1c7a0d555
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-23 15:38:13 +0300

    Initial commit 5.8 Triangle

---
+++ WS5_8_Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WS5_8_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that can print out a triangle. 
+            //First the program asks for the size of the triangle
+            Console.WriteLine("Size of triangle: ");
+            int size = int.Parse(Console.ReadLine());
+            // Size determines how many rows and columns at the bottom the triangle will have.
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
