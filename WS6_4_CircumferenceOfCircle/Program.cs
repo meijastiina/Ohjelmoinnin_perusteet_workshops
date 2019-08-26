dc1ea4a3cbf54421ff534218ddb666bdd654c63b
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-26 08:33:57 +0300

    Initial commitWS 6.4 Circumference of a Circle

---
+++ WS6_4_CircumferenceOfCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WS6_4_CircumferenceOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call this method three times with different parameters.
+            Console.WriteLine("Circumference of a circle when radius is 1: " + CircumferenceOfACircle(1));
+            Console.WriteLine("Circumference of a circle when radius is 1.567: " + CircumferenceOfACircle(1.567));
+            Console.WriteLine("Circumference of a circle when radius is 1567: " + CircumferenceOfACircle(1567));
+        }
+        // Create a program that calculates the circumference of a circle when radius is given(circumference = 2 * π * radius). 
+        private static double CircumferenceOfACircle(double radius)
+        {
+            return 2 * Math.PI * radius; // You can use Math class to get the value of π.
+        }
+    }
+}
