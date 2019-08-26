5510d74a994541de507a1e82c1cb9add92187492
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-08-26 08:08:28 +0300

    Initial commit WS 6.3 Sum of Numbers, Renamed Hangman -> WS5_9_Hangman

---
+++ WS6_3_SumOfNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WS6_3_SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call this method in your main method, save the return value in a variable and print out. Do this three times with different variable values. 
+            int sum = Sum(1, 3);
+            Console.WriteLine("1 + 3 = " + sum);
+            Console.WriteLine("510 + 245 = " + Sum(510, 245));
+            Console.WriteLine("89 + -12 = " + Sum(89, -12));
+        }
+        // Create a program that has a method Sum(int firstNumber, int secondNumber) that returns the sum of the numbers passed to the method as input parameters. 
+        private static int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
