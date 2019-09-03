d5d7bebdfd0ff82aad865e16b6cada0729eda3cf
meijastiina <mlohiniva@hotmail.com>
2019-09-03 11:09:09 +0300

    Initial commit WS 7.4 Working with Indices

---
+++ WS7_4_WorkingWithIndices/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WS7_4_WorkingWithIndices
+{
+    /* 
+     * Create a program that asks for grades (on the scale 0-5) for 21 students and counts the number of each grade. 
+     * After the grades have been entered the program prints out how many of each grades were given using asterisks. 
+     * In this exercise you’re only interested in the number of each grade (How many fives there were etc), 
+     * no need to store the data on specific student’s grade. 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[6]; // Scale 0-5 -> We need 6 items
+            int grade; // This is used for storing the input
+            for (int i = 0; i < 21; i++) // Loops through the 21 students
+            {
+                Console.Write($"Grade for student { i + 1 }: ");
+                grade = int.Parse(Console.ReadLine());
+                grades[grade] = grades[grade]+1; // Increments the number of this specific grade
+            }
+            // All the grades have been entered
+            for (int i = 0; i < grades.Length; i++) // Loops through the grades 0-5
+            {
+                Console.Write($"{ i }:");
+                for (int j = 0; j < grades[i]; j++) // Adds as many asterisks as the value in this index
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); // Next grade on a new line
+            }
+        }
+    }
+}
